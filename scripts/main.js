0e5633d310c2335b9d34c845560b53a236f2db61
Student <tatevik.harutyunyan23@gmail.com>
2016-04-08 18:00:09 +0400

    Author Tatevik Created core, single, edit, and add pages.

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+ $( document ).ready(function() {
+ 	$("#core").load("core.html");
+    $("#core").on("click", "#editdata", function(){
+    	$("#core").empty();	
+    	$("#add").empty();	
+    	$("#single").empty();
+    	$("#edit").load('edit.html'); 
+    });
+ 	$("a").click(function(){
+ 		$("#core").empty();	
+ 		$("#edit").empty();
+ 		$("#single").empty();
+    	$("#add").load("add.html"); 
+    });
+ 	$("#logoId").click(function(){
+ 		$("#add").empty();
+ 		$("#edit").empty();
+ 		$("#single").empty();
+ 		$("#core").load("core.html");
+ 	});
+ 	$("#core").on("click", ".name", function(){
+ 		$("#add").empty();
+ 		$("#edit").empty();
+ 		$("#core").empty();	
+ 		$("#single").load("single.html");
+ 	});
+
+});

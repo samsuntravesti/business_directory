4ca92aadcf222cd000d93aeb3373e3d169791a4c
Student <tatevik.harutyunyan23@gmail.com>
2016-04-12 17:09:58 +0400

    Author Tatevik Created API which get data from db

---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$user_name = "root";
+$password = "student";
+$database = "businessDirectory";
+$server = "127.0.0.1";
+	function database(){
+		global $user_name, $password, $database, $server;
+		$mysqli = new mysqli($server, $user_name, $password, $database);
+			echo '<meta charset="utf-8">';
+
+			$sql = 'SELECT *  FROM businessDirectory.mytable';
+
+			$collation="SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'";
+			$mysqli->query($collation);
+			$result = $mysqli->query($sql);
+			$arr = array();
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		    	array_push($arr, $row);
+		    }
+		} else {
+		    echo "0 results";
+		}
+		$mysqli->close();
+		return json_encode($arr, JSON_UNESCAPED_UNICODE);
+}
+echo database();
+?>

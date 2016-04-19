1856e5a649ad88e72eb956a429f9b68efd538a8c
Student <tatevik.harutyunyan23@gmail.com>
2016-04-19 17:43:44 +0400

    Author: Tatevik Add data in database

--- api/add.php
+++ api/add.php
@@ -1,29 +1,43 @@
 <?php
-//echo($_SERVER['REQUEST_METHOD']);
-	$title =  $_GET["title"];
-	 //print_r($_GET["title"]);
+	//if(isset($_POST['add_product'])){
+
+//print_r($_GET);
 	$user_name = "root";
 	$password = "student";
 	$database = "businessDirectory";
 	$server = "127.0.0.1";
-	function database($user_name, $password, $database, $server, $title){
+	function database($user_name, $password, $database, $server){
 		$mysqli = new mysqli($server, $user_name, $password, $database);
 		//echo '<meta charset="utf-8">';
-		$sql = 'INSERT INTO businessDirectory.mytable (`number`, `name`, `type`, `region`, `address`, `activity`, `detalis`, `website`, `phone`, `email`, `president`) VALUES('$_POST["inputId"]', '$_POST["inputName"]', '$_POST["inputType"]', '$_POST["inputRegion"]', '$_POST["inputAddress"]', '$_POST["inputAct"]', '$_POST["inputActivityType"]', '$_POST["inputWeb"]', '$_POST["inputTel"]', '$_POST["inputMail"]', '$_POST["inputDir"]',)';
+		$number = $_GET['number'];
+		$name = $_GET['name'];
+		$type = $_GET['inputType'];
+		$region = $_GET['inputRegion'];
+		$address = $_GET['inputAddress'];
+		$activity = $_GET['inputAct'];
+		$detalis = $_GET['inputActivityType'];
+		$website = $_GET['inputWeb'];
+		$phone = $_GET['inputTel'];
+		$email = $_GET['inputMail'];
+		$president = $_GET['inputDir'];
+		$sql = 'insert INTO businessDirectory.mytable (number, name, type, region, address, activity, details, website, phone, email, president) values (\''.$number. '\' , \''.$name. '\', \''.$type. '\', \''.$region. '\', \''.$address. '\', \''.$activity. '\', \''.$detalis. '\', \''.$website. '\', \''.$phone. '\', \''.$email. '\', \''.$president. '\')';
+		//$sql = 'INSERT INTO businessDirectory.mytable (`number`, `name`, `type`, `region`, `address`, `activity`, `detalis`, `website`, `phone`, `email`, `president`) VALUES('$_POST["inputId"]', '$_POST["inputName"]', '$_POST["inputType"]', '$_POST["inputRegion"]', '$_POST["inputAddress"]', '$_POST["inputAct"]', '$_POST["inputActivityType"]', '$_POST["inputWeb"]', '$_POST["inputTel"]', '$_POST["inputMail"]', '$_POST["inputDir"]',)';
+		echo $sql;
 		$collation="SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'";
 		$mysqli->query($collation);
 		$result = $mysqli->query($sql);
 		$arr = array();
-		if ($result->num_rows > 0) {
+		/*if ($result->num_rows > 0) {
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
 		    	array_push($arr, $row);
 		    }
 		} else {
 		    echo "0 results";
-		}
+		}*/
 		$mysqli->close();
-		return json_encode($arr, JSON_UNESCAPED_UNICODE);
+		//return json_encode($arr, JSON_UNESCAPED_UNICODE);
 	}
-	echo database($user_name, $password, $database, $server, $title);
+	echo database($user_name, $password, $database, $server);
+//}
 ?>
--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
           var i, n;
           var obj = jQuery.parseJSON(data);
           for (i = 0; i < obj.length; ++i) {
-            $('#resultId ol').append('<li><div class="info"><a onclick="getCompanyData(' + obj[i].number + ');" class="name" id="' + obj[i].number + '">' + obj[i].name + '</a><span class="glyphicon glyphicon-pencil" onclick="editCompany(' + obj[i].number + ')" id="' + obj[i].number + '" style="float: right; margin-right: 3%"></span><span onclick="deleteCompany(' + obj[i].number + ');" class="glyphicon glyphicon-trash" style="float: right" id="' + obj[i].number + '"></span></div></li>');
+            $('#resultId ol').append('<li><div class="info"><a onclick="getCompanyData(' + obj[i].number + ');" class="name" id="' + obj[i].number + '">' + obj[i].name + '</a><span class="glyphicon glyphicon-pencil" onclick="editCompany(' + obj[i].number + ')" id="' + obj[i].number + '" style="float: right; margin-right: 5%"></span><span onclick="deleteCompany(' + obj[i].number + ');" class="glyphicon glyphicon-trash" style="float: right" id="' + obj[i].number + '"></span></div></li>');
           }
           $("#search").val("");
         }
@@ -65,22 +65,25 @@ function getCompanyData(number) {
       data: "number=" + number,
       success: function(data){
         var data = jQuery.parseJSON(data);
-        console.log(data);
-        console.log(data[0].region);
+       // console.log(data);
+      //  console.log(data[0].region);
         $('#firmName').html(data[0].name);
         $('#regionId').html(data[0].region);
         $('#addressId').html(data[0].address);
         $('#phoneId').html(data[0].phone);
-        $('#dirId').html(data[0].director);
-        $('#postId').html(data[0].post);
-        $('#webId').html(data[0].web);
+        $('#dirId').html(data[0].president);
+        $('#postId').html(data[0].email);
+        $('#webId').html(data[0].website);
         $('#activityId').html(data[0].activity);
-        $('#actId').html(data[0].act);
+        $('#actId').html(data[0].details);
         $('#typeId').html(data[0].type);
 
       }
     });
   console.log(number);
+}
+function refresh(){
+  $("#core").load("core.html");
 }
 function deleteCompany(number){
   $.ajax({
@@ -89,18 +92,18 @@ function deleteCompany(number){
       data: "number=" + number,
       success: function(data){
         alert("You have just deleted 1 item!");
+        refresh();
       }
   });
 }
 function editCompany(number){
-  single();
   $.ajax({
-    url: "api/single.php",
+    url: "api/add.php",
     type: "get",
     data: "number=" + number,
     dataType: "json",
     success: function(data){
-      
+     
     }
   });
 }
@@ -111,26 +114,30 @@ function add(){
   $("#add").load("add.html");
 }
 function addCompany(){
-    $('#add_company').click(function() {
-      alert('min pen');
-  //    var id = $('#inputId').val();
-   //   var name = $('#inputName').val();
- //     var type = $('#sel1').val();
-  //    var actType = $('#sel2').val();
-  //    var region = $('#sel3').val();
-   //   var act = $('#sel4').val();
-   //   var address = $('#inputAddress').val();
-   //   var website = $('#inputWeb').val();
-   //   var phone = $('#inputTel').val();
-    //  var email = $('#inputMail').val();
-   //   var direct = $('#inputDir').val();
- //     $.ajax({
- //         type: "POST",        
-   //       url: "api/add.php",
-    //      data: {"number": id, 'name=':name, 'inputType=': type, 'inputActivityType=': actType, 'inputRegion=': region, 'inputAct=': act, 'inputAddress=': address, 'inputWeb=': website, 'inputTel=': phone, 'inputMail=': email, 'inputDir=': direct},
-     //     success: function(data) {
-    //          alert('data has been stored to database');
-       //   }
+//    $('#add_company').click(function() {
+     
+      var id = $('#inputId').val();
+      var name = $('#inputName').val();
+      var type = $('#sel1').val();
+      var actType = $('#sel2').val();
+      var region = $('#sel3').val();
+      var act = $('#sel4').val();
+      var address = $('#inputAddress').val();
+      var website = $('#inputWeb').val();
+      var phone = $('#inputTel').val();
+      var email = $('#inputMail').val();
+      var direct = $('#inputDir').val();
+      $.ajax({
+          type: "GET",        
+          url: "api/add.php",
+          data: {'number': id, 'name':name, 'inputType': type, 'inputActivityType': actType, 'inputRegion': region, 'inputAct': act, 'inputAddress': address, 'inputWeb': website, 'inputTel': phone, 'inputMail': email, 'inputDir': direct},
+          success: function(data) {
+              alert('data has been stored to database');
+              $( '#formId' ).each(function(){
+                this.reset();
+              });
+          }
       });
- // });
+  //});
 }
+

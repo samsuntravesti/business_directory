44813d6e7a4c0174a7e8fc930eda2441e573d5ee
Shoghik <git@qarintak.(none)>
2016-09-06 12:26:57 +0400

    Author: Tatevik Clean code

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('/home/git/Desktop/businessDirectory/api/config.php');
+ 	require_once(SITE_URI . '/header.php');
+ 	require_once(SITE_URI . '/api/functions.php');
+ 	$delete = delete();
+ 	/*print_r($delete);
+ 	echo '<br>';*/
+ 	$number = $_GET['number'];
+ 	//echo $number;
+ 	
+?>
+<h1 style="text-align: center">1 item is deleted!</h1>
+
+<?php
+ 	require_once(SITE_URI . '/template/footer.php');
+?>	
